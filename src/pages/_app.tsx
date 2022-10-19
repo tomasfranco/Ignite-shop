2f3d0caea3853fe3b575de3e1b132fe582b67089
Tomas Franco <tomasfranco@Tomas-MacBook-Pro.local>
2022-10-19 01:13:11 -0300

    Imagens no Next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { globalStyles } from "../styles/global"
 import logoImg from "../assets/logo.svg";
 import { Container, Header } from "../styles/pages/app";
 
+import Image from 'next/future/image';
 
 globalStyles();
 
@@ -13,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
     <Component {...pageProps} /> 
   </Container>
