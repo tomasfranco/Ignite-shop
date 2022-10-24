763ecba9a1e2f025c440a52aa8418e1bb38777b6
Tomas Franco <tomasfranco@Tomas-MacBook-Pro.local>
2022-10-24 00:30:14 -0300

    formatando o preco

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,7 +62,10 @@ export default function Home({ products }: HomeProps) {
         id: product.id,
         name: product.name,
         imageUrl: product.images[0],
-        price: price.unit_amount / 100,
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
       }
     })
 
