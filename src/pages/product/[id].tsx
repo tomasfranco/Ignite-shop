5c292184a88ffbf4f73421455efe4eeee12d401e
Tomas Franco <tomasfranco@Tomas-MacBook-Pro.local>
2022-10-25 23:46:42 -0300

    fallback do SSG

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from 'next/future/image';
+import { useRouter } from "next/router";
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
@@ -15,7 +16,11 @@ interface ProductProps {
 }
 
 export default function Product({product}: ProductProps) {
-  
+  const { isFallback } = useRouter() 
+
+  if(isFallback) {
+    return <p>Loading...</p>
+  }
 
   return (
     <ProductContainer>
