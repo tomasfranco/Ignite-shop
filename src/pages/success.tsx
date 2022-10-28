a3420c338b4566ab3602815ae94341b2f0bd2dc8
Tomas Franco <tomasfranco@Tomas-MacBook-Pro.local>
2022-10-27 23:03:59 -0300

    Produto & Checkout - Estrutura de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import { SuccessContainer, ImageContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+
+      </ImageContainer>
+
+    <p>Uhuul <strong>Tomás Franco</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.</p>
+
+    <Link href="/">
+      Voltar ao catálogo
+    </Link>
+    </SuccessContainer>
   )
 }
